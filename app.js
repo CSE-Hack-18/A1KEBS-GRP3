9028bd1c0fdb9cd95d3dfe1cf8d3bcf2ce9b7ec1
andres <gusdebana@student.gu.se>
2018-05-05 12:09:29 +0200

    App.js

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express')
+var path = require('path')
+var io = require('socket.io')(8080);
+
+var mongo = require('./server/mongo.js')
+
+var app = express()
+
+app.use(express.static('client'));
+
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/client/home.html'))
+})
+
+app.get('/client', function (req, res) {
+    res.sendFile(path.join(__dirname + '/client/client.html'))
+})
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000.")
+})
+
+io.on('connection', function (client) {
+    console.log("A new client has connected...");
+
+    client.on('register', function (data) {
+        console.log("A new user has registered.");
+        console.log(data);
+        //mongo.saveUser(data);
+    });
+
+
+});
