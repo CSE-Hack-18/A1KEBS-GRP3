fa72d820f3c23ee85cb636774bc84d1012c5f422
Mantas Namgaudis <namgaudis@gmail.com>
2018-05-06 01:57:26 +0200

    updating client

--- client/index.js
+++ client/index.js
@@ -1,15 +1,33 @@
 console.log('index javascript working');
 
-var socket = io("http://localhost:8080");
+$(function(){
+	// initialize modal
+	$('.modal').modal();
 
+	$('#button1').on('click', () => {
 
-//on load
-() => {
-	socket.emit('getUnit', 'generalMedicine', (data) => {
-		console.log(data);
-	});
-}
+	})
 
-socket.on('updateRoom', (data) => {
-	
+	$('#button2').on('click', () => {
+		$('#modal-room').modal('open');
+	})
+
+	$('.room').on('click', (event) => {
+		var room = event.target.children[0].textContent;
+		$('#modal-header').text(room);
+		$('#modal-room').modal('open');
+	})
+
+	$('.treatment-room').on('click', (event) => {
+		$('#modal-treatment-room').modal('open');
+	})
+
+	$('.staff-room').on('click', (event) => {
+		$('#modal-staff-room').modal('open');
+	})
 });
+
+// add OR remove
+function addToRoom(room) {
+
+}
