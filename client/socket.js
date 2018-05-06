129e9604309f97f7fc8ce01fe6807e4657e74d7e
elkbot <elkbot@users.noreply.github.com>
2018-05-06 09:42:28 +0200

    update bed occupancy

--- client/socket.js
+++ client/socket.js
@@ -1,28 +1,31 @@
 var socket = io("http://localhost:8080");
 
-
 var beds = [];
 
 socket.emit('getBeds', 'General Medicine', function (data) {
     beds = data;
     for (var i = 0; i < beds.length; i++) {
-        console.log(beds[i]);
-        getPersonForBed(i, function (i, e) {
-            beds[i].patient = e;
-            if(beds[i].occupied){
-                addPatientToBed(beds[i].number);
-            }
-        });
+        getPersonForBed(i);
     }
 });
 
-function getPersonForBed(i, callback) {
+function getPersonForBed(i) {
     socket.emit('getBedOwner', beds[i].number, function (data) {
-        callback(i, data);
+        beds[i].patient = data;
+            if(beds[i].occupied){
+                addPatientToBed(beds[i].number);
+            }
     });
 }
 
-
 socket.on('updateRoom', (data) => {
-    //fillRoom(bed.number);
+    var location = getBedLocation(data);
+    getPersonForBed(location);
 });
+
+function getBedLocation(bedNumber){
+    var bed = $.grep(beds, function(index){
+        return index.number === bedNumber})[0];
+    var index = beds.indexOf(bed);
+    return index;
+}
